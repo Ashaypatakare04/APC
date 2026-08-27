bbedad45c9fa5e0b244792e8b291e951403507a6
Ashaypatakare04 <patakareashay@gmail.com>
2026-08-27 22:34:40 +0530

    Implement frozenset operations and properties
    Added various operations and properties for frozensets.

--- EXP_6/frozen.py
+++ EXP_6/frozen.py
@@ -1,2 +1,29 @@
-frozen=frozenset([1,2,3,4,5])
-print(type(frozen))
+A = frozenset([1, 2, 3, 4, 5])
+B = frozenset([4, 5, 6, 7, 8])
+
+print("A =", A)
+print("B =", B)
+
+print("Length of A:", len(A))
+
+print("3 in A:", 3 in A)
+print("10 in A:", 10 in A)
+
+print("Union:", A.union(B))
+print("Intersection:", A.intersection(B))
+print("Difference:", A.difference(B))
+print("Symmetric Difference:", A.symmetric_difference(B))
+
+C = frozenset([1, 2, 3])
+print("Subset:", C.issubset(A))
+print("Superset:", A.issuperset(C))
+
+D = frozenset([10, 20, 30])
+print("Disjoint:", A.isdisjoint(D))
+
+E = A.copy()
+print("Copy:", E)
+
+print("Maximum:", max(A))
+print("Minimum:", min(A))
+print("Sum:", sum(A))
