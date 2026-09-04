5c1bd60f1fc895f2ccec4629317c358ada72f7bb
Ashaypatakare04 <patakareashay@gmail.com>
2026-09-04 15:37:55 +0530

    Add files via upload

--- EXP_8/bank.py
+++ EXP_8/bank.py
@@ -1,7 +1,7 @@
 class bank:
-    def __init__(self,name,bal,acc_no):
+    def __init__(self,name,acc_no):
         self.name=name
-        self.bal=bal
+        self.bal=0
         self.acc_no=acc_no
         
     def deposit(self,n):
@@ -22,18 +22,21 @@ class bank:
         pass
     
 class acc1(bank):
-    def __init__(self,n,b,acc):
-        super().__init__(n,b,acc)
+    def __init__(self,n,acc):
+        super().__init__(n,acc)
 
 class acc2(bank):
-    def __init__(self,n,b,acc):
-        super().__init__(n,b,acc)
-obj1=acc1("Customer 1",10000,12345678910)
+    def __init__(self,n,acc):
+        super().__init__(n,acc)
+        
+obj1=acc1("Customer 1",12345678910)
 obj1.check_balance()
 obj1.deposit(5000)
+obj1.withdraw(1000)
 obj1.check_balance()
 
-obj2=acc2("Customer 2",500000,9876543210)
+obj2=acc2("Customer 2",9876543210)
 obj2.check_balance()
 obj2.deposit(50000)
+obj1.withdraw(1000)
 obj2.check_balance()
